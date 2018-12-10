3de5918a4f9186e7d31e18a6b4895f6cc455695c
David C Wang <dcwangmit01@gmail.com>
2018-12-09 16:00:56 -0800

    Updated README

--- README.md.sh
+++ README.md.sh
@@ -8,12 +8,29 @@ cat > README.md <<EOF
 
 Tool for deploying a remotely-accessible single node kubernetes cluster on linux using kubeadm.
 
-## Install
+## Install the script (For use with existing Linux machine)
 
 \`\`\`
 sudo curl -o /usr/local/bin/kubeadm-one \\
   https://raw.githubusercontent.com/cisco-sso/kubeadm-one/master/kubeadm-one && \\
   sudo chmod 755 /usr/local/bin/kubeadm-one
+\`\`\`
+
+## Install via Vagrant (Creates a new Virtual Machine and Installs Kubernetes)
+
+\`\`\`
+git clone https://github.com/cisco-sso/kubeadm-one.git
+cd kubeadm-one
+
+# Customize the Vagrantfile settings
+#  \$address = "" # Set this if you want to set a static IP alias on eth0 (e.g. "192.168.3.13")
+#  \$fqdn = ""    # Set this if you will access the kube cluster remotely (e.g. "api.kube.example.com)
+
+# For OSX Virtualbox
+vagrant up
+
+# For Windows Hyper-V
+vagrant up --provider hyperv
 \`\`\`
 
 ## Usage
